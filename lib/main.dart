d5b1153c436dc964fa13a01b3c2d1f0a3cbc447c
Jaewon Choi <jaewon.james.choi@gmail.com>
2018-08-02 23:58:58 +0900

    Refactoring for stateful feature

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:sensors/sensors.dart';
 
 Future<String> fetchTodayInformation() async {
   final response = await http.get('http://10.0.1.5:8080/today');
@@ -20,26 +21,42 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'notifier',
-      home: Scaffold(
-        backgroundColor: const Color.fromARGB(255, 255, 0, 0),
-        body: Center(
-          child: FutureBuilder<String>(
-            future: fetchTodayInformation(),
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return Text(
-                  snapshot.data,
-                  style: new TextStyle(
-                    color: Colors.white,
-                    fontSize: 50.0
-                  ));
-              } else if (snapshot.hasError) {
-                return Text("${snapshot.error}");
-              }
+      home: new MyHomePage(title: 'notifier')
+    );
+  }
+}
 
-              return CircularProgressIndicator();
-            })
-        ),
+class MyHomePage extends StatefulWidget {
+  final String title;
+
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      backgroundColor: const Color.fromARGB(255, 255, 0, 0),
+      body: Center(
+          child: FutureBuilder<String>(
+              future: fetchTodayInformation(),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return Text(
+                      snapshot.data,
+                      style: new TextStyle(
+                          color: Colors.white,
+                          fontSize: 50.0
+                      ));
+                } else if (snapshot.hasError) {
+                  return Text("${snapshot.error}");
+                }
+
+                return CircularProgressIndicator();
+              })
       ),
     );
   }
